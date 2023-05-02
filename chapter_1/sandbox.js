3ab5937fc131dc52c3003b2feb7b9c9fc8625897
sooto selim <adewaleselim6@gmail.com>
2023-05-02 19:37:55 +0100

    learning 01\

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,1 +1,6 @@
 alert("hello world"); //  we use semicolon to mark the end of a stateent just like . ending the line of a sentence
+
+// javascript run code from top to bottom
+console.log(1);
+console.log(2);
+// the console is a function to log out value to you browser console, it just use for testing
