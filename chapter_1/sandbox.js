e9b51b929900d4ac4edcb13e07cc0d5b94238da4
sooto selim <adewaleselim6@gmail.com>
2023-05-02 19:41:45 +0100

    learning 02

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -4,3 +4,11 @@ alert("hello world"); //  we use semicolon to mark the end of a stateent just li
 console.log(1);
 console.log(2);
 // the console is a function to log out value to you browser console, it just use for testing
+
+// lets talk about variable
+// basically what variable do is to give us a way to store VALUES eg name, email, number and then use that varibale whenever we want to
+// we have differnt way of making var
+// example 1
+
+let age = 20;
+console.log(age);
